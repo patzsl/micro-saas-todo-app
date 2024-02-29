a6eb1009964a91b0806a280892d8863cdd56c92a
P̴̻̅̉a̷͙͈̜͋̀t̷̟̓͠z̴̝̣̰͋͑ ̶̲̋̃̔ś̷͓̥̰L̸̖̗̼͑̒̀ <gm.teixeira@icloud.com>
2024-02-29 14:39:36 -0300

    initial commit

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,9 @@
+import { AuthForm } from "./_components/auth-form";
+
+export default function Page() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <AuthForm/>
+        </main>
+    )
+}
