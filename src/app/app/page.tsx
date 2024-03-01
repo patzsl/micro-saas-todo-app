8dafbae079d637c07e042e8fff4eae4f338a6e23
P̴̻̅̉a̷͙͈̜͋̀t̷̟̓͠z̴̝̣̰͋͑ ̶̲̋̃̔ś̷͓̥̰L̸̖̗̼͑̒̀ <gm.teixeira@icloud.com>
2024-02-29 21:17:53 -0300

    mudanças na pagina principal do app

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -1,12 +1,22 @@
 import { auth } from "@/services/auth"
 import { UserInfo } from "./_components/user-info"
+import RoomPlayer from "../player/player"
 
 export default async function Page() {
   const session = await auth()
   
   return (
-    <main className="flex items-center justify-center h-screen">
+    <main className="flex flex-col items-end h-screen p-4">
       <UserInfo user={session?.user} />
+      {/* <img
+        alt="Hero"
+        className="mx-auto aspect-video overflow-hidden rounded-xl object-cover object-center"
+        height="600"
+        src="/placeholder.svg"
+        width="1200"
+      /> */}
+      <RoomPlayer/>
+
     </main>
   )
 }
