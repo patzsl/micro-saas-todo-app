c47dfd99857a8401d24bd029f52c1b649b7d1f7f
P̴̻̅̉a̷͙͈̜͋̀t̷̟̓͠z̴̝̣̰͋͑ ̶̲̋̃̔ś̷͓̥̰L̸̖̗̼͑̒̀ <gm.teixeira@icloud.com>
2024-02-29 22:16:13 -0300

    voltar inicio

--- src/app/app/_components/user-info.tsx
+++ src/app/app/_components/user-info.tsx
@@ -6,6 +6,7 @@ import { signOut } from "next-auth/react";
 import { UserAvatar } from "./avatar";
 import RoomPlayer from "@/app/player/player";
 import { Input } from "@/components/ui/input"
+import Link from "next/link";
 
 
 type Props = {
@@ -17,7 +18,7 @@ export function UserInfo({ user }: Props) {
 
   return (
     <div className="w-full flex flex-row items-center justify-center justify-between gap-4">
-      <h1>share room</h1>
+      <Link href="/">share room</Link>
       <Input className="w-1/2 rounded-md" placeholder="Paste YouTube link here" type="text" />
       <button className="cursor-pointer" onClick={() => signOut()}>
         <UserAvatar />
